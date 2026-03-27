0bea3a61c4960844ccab72d6ed1ef029b2b16291
Robby Awalul <robbyawalulma@gmail.com>
2026-03-27 09:27:32 +0700

    Initial Commit

---
+++ iOS Expert/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS Expert
+//
+//  Created by Robby Awalul on 27/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS Expert/iOS_ExpertApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_ExpertApp.swift
+//  iOS Expert
+//
+//  Created by Robby Awalul on 27/03/26.
+//
+
+import SwiftUI
+
+@main
+struct iOS_ExpertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
